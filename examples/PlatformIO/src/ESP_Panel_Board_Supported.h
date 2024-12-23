a62b35a21a566a547a6e83508d65694364aea434
VIEWESMART <liulong085711@outlook.com>
2024-12-23 18:57:43 +0800

    Update ESP_Panel_Board_Supported.h

--- examples/PlatformIO/src/ESP_Panel_Board_Supported.h
+++ examples/PlatformIO/src/ESP_Panel_Board_Supported.h
@@ -7,7 +7,7 @@
 #pragma once
 
 /* Set to 1 if using a supported board */
-#define ESP_PANEL_USE_SUPPORTED_BOARD       (0)         // 0/1
+#define ESP_PANEL_USE_SUPPORTED_BOARD       (1)         // 0/1
 
 #if ESP_PANEL_USE_SUPPORTED_BOARD
 /**
